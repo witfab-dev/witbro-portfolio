fe09ac1126077801928422159b8a7c4a1356833f
wit_fab <155532341+witfab-dev@users.noreply.github.com>
2026-05-23 19:43:15 +0000

    main updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,tsx,jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
